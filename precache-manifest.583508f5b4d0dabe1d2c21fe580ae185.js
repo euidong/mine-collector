88bab5460930932bd29dea221be1560457da4314
euidong <48043626+euidong@users.noreply.github.com>
2020-04-28 01:57:53 +0000

    Deploying to gh-pages from  @ b695027e2455be131f72b23a182ac073908e600b 🚀

---
+++ precache-manifest.583508f5b4d0dabe1d2c21fe580ae185.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fda0a9e1ea0a548cb1d6d1e09ff8c0f2",
+    "url": "/euidong.github.io/mine-front/index.html"
+  },
+  {
+    "revision": "37569041e993d637fe6d",
+    "url": "/euidong.github.io/mine-front/static/css/main.0b69781b.chunk.css"
+  },
+  {
+    "revision": "2da4626ebebbb9ed59ce",
+    "url": "/euidong.github.io/mine-front/static/js/2.624bdd01.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/euidong.github.io/mine-front/static/js/2.624bdd01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37569041e993d637fe6d",
+    "url": "/euidong.github.io/mine-front/static/js/main.239034c7.chunk.js"
+  },
+  {
+    "revision": "3900ef89f780b7d5e986",
+    "url": "/euidong.github.io/mine-front/static/js/runtime-main.e6ae8fd2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bfe940867709ca5300ec4eaa9e7b3545.js"
+  "/euidong.github.io/mine-front/precache-manifest.583508f5b4d0dabe1d2c21fe580ae185.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/euidong.github.io/mine-front/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
