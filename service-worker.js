1e97273366810886cb5868552f965d4ccf407afd
euidong <justicedong@naver.com>
2020-04-28 11:19:48 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mine-collector/precache-manifest.a2ab7b6de0dac4bff864838c01ae4fe8.js"
+  "/mine-collector/precache-manifest.e2b7d9dd41dfae2932ab17d73769a101.js"
 );
 
 self.addEventListener('message', (event) => {
